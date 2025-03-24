2927c6af4937bb35f1c4a1c287058a29ca389a73
21CFV01ChristFlores <c.flores.1ddb@gmail.com>
2025-03-24 11:56:45 -0600

    Add files via upload

---
+++ Guia1_24_marzo_7_ChristFlores/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Guia1_24_marzo_7_ChristFlores
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            Numero = new DataGridViewTextBoxColumn();
+            Multi = new DataGridViewTextBoxColumn();
+            Total = new DataGridViewTextBoxColumn();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(60, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Digite un valor:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(179, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(202, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(421, 59);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Ver";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Numero, Multi, Total });
+            dataGridView1.Location = new Point(49, 124);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(343, 213);
+            dataGridView1.TabIndex = 3;
+            // 
+            // Numero
+            // 
+            Numero.HeaderText = "numero";
+            Numero.Name = "Numero";
+            // 
+            // Multi
+            // 
+            Multi.HeaderText = "Multiplicacion";
+            Multi.Name = "Multi";
+            // 
+            // Total
+            // 
+            Total.HeaderText = "total";
+            Total.Name = "Total";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(450, 358);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Ejercicio 1";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(593, 416);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Ciclos";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Numero;
+        private DataGridViewTextBoxColumn Multi;
+        private DataGridViewTextBoxColumn Total;
+        private Button button2;
+    }
+}
---
+++ Guia1_24_marzo_7_ChristFlores/Form3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia1_24_marzo_7_ChristFlores
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int cont = 1;
+
+            while (cont < 11)
+            {
+                int multi;
+                multi = num1 * cont;
+                dataGridView1.Rows.Add(num1, cont, multi);
+                cont++;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form4 form = new Form4();
+            form.ShowDialog();
+        }
+    }
+}
